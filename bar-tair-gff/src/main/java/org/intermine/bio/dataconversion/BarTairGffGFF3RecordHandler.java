c0c437657c9f61677a037f7b00af53d40ccf1ed9
asherpasha <asher.pasha@utoronto.ca>
2019-05-17 21:38:21 -0400

    Using Vivek's GFF3 loader.

--- bar-tair-gff/src/main/java/org/intermine/bio/dataconversion/BarTairGffGFF3RecordHandler.java
+++ bar-tair-gff/src/main/java/org/intermine/bio/dataconversion/BarTairGffGFF3RecordHandler.java
@@ -1,7 +1,7 @@
 package org.intermine.bio.dataconversion;
 
 /*
- * Copyright (C) 2002-2016 FlyMine
+ * Copyright (C) 2002-2017 Araport
  *
  * This code may be freely distributed and modified under the
  * terms of the GNU Lesser General Public Licence.  This should
@@ -10,26 +10,38 @@ package org.intermine.bio.dataconversion;
  *
  */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+//import org.apache.log4j.Logger;
 import org.intermine.bio.io.gff3.GFF3Record;
 import org.intermine.metadata.Model;
 import org.intermine.metadata.StringUtil;
 import org.intermine.xml.full.Item;
 
 /**
- * A converter/retriever for the BarTairGff dataset via GFF files.
+ * A converter/retriever for the AraportGff dataset via GFF files.
  */
 
-public class BarTairGffGFF3RecordHandler extends GFF3RecordHandler {
+public class BarTairGffGFF3RecordHandler extends GFF3RecordHandler
+{
+
+    //private static final Logger LOG = Logger.getLogger(BarTairGffGFF3RecordHandler.class);
+    private final Map<String, Item> pubmedIdMap = new HashMap<String, Item>();
+    private final Map<String, Item> protIdMap = new HashMap<String, Item>();
     /**
      * Create a new BarTairGffGFF3RecordHandler for the given data model.
-     *
      * @param model the model for which items will be created
      */
-    public BarTairGffGFF3RecordHandler(Model model) {
+    public BarTairGffGFF3RecordHandler (Model model) {
         super(model);
         refsAndCollections.put("Exon", "transcripts");
-        refsAndCollections.put("CDS", "transcripts");
         refsAndCollections.put("Transcript", "gene");
         refsAndCollections.put("LncRNA", "gene");
         refsAndCollections.put("MRNA", "gene");
@@ -57,67 +69,94 @@ public class BarTairGffGFF3RecordHandler extends GFF3RecordHandler {
         // the key.
         Item feature = getFeature();
         String clsName = feature.getClassName();
-        String type = record.getType();
 
-        // The type column in GFF3
-        switch (type) {
-            case "gene":
-                // Set Araport/TAIR gene id
-                feature.setClassName("Gene");
+        // For the following feature classes, check if the GFF3 attribute `full_name`, is defined
+        // If true, assign its value to the InterMine attribute `name`
+        String regexp = "Gene|MRNA|TransposableElement|TransposableElementGene";
+        Pattern p = Pattern.compile(regexp);
+        Matcher m = p.matcher(clsName);
+        if (m.find()) {
+            if (record.getAttributes().get("full_name") != null) {
+                String fullName = record.getAttributes().get("full_name").iterator().next();
+                feature.setAttribute("name", fullName);
+            }
+        }
 
-                // This might not be needed.
-                String geneID = record.getAttributes().get("ID").iterator().next();
-                feature.setAttribute("primaryIdentifier", geneID);
+        // For the following feature classes, check if the Dbxref(s) to the TAIR
+        // `locus` and `gene` are defined. If true, assign their values to the
+        // InterMine attribute `secondaryIdentifier`
+        //
+        // Also,  check if there are Dbxref(s) to PubMed identifiers. If true,
+        // assign their values to the `Publication` entity of the data model
+        regexp = "Gene|MRNA|Pseudogene|TransposableElementGene";
+        p = Pattern.compile(regexp);
+        m = p.matcher(clsName);
+        if (m.find()) {
+            //String primaryIdentifier = feature.getAttribute("primaryIdentifier").getValue();
 
-                // This is gene alias
-                if (record.getAttributes().get("symbol") != null) {
-                    String symbol = record.getAttributes().get("symbol").iterator().next();
-                    feature.setAttribute("symbol", symbol);
+            List<String> dbxrefs = record.getDbxrefs();
+            if (dbxrefs != null) {
+                Iterator<String> dbxrefsIter = dbxrefs.iterator();
+
+                while (dbxrefsIter.hasNext()) {
+                    String dbxref = dbxrefsIter.next();
+
+                    List<String> refList = new ArrayList<String>(
+                            Arrays.asList(StringUtil.split(dbxref, ",")));
+                    for (String ref : refList) {
+                        ref = ref.trim();
+                        int colonIndex = ref.indexOf(":");
+                        if (colonIndex == -1) {
+                            throw new RuntimeException("external reference not understood: " + ref);
+                        }
+
+                        if (ref.startsWith("gene:") || ref.startsWith("locus:")) {
+                            feature.setAttribute("secondaryIdentifier", ref);
+                        } else if (ref.startsWith("PMID:")) {
+                            String pmid = ref.substring(colonIndex + 1);
+                            Item pubmedItem;
+                            if (pubmedIdMap.containsKey(pmid)) {
+                                pubmedItem = pubmedIdMap.get(pmid);
+                            } else {
+                                pubmedItem = converter.createItem("Publication");
+                                pubmedIdMap.put(pmid, pubmedItem);
+                                pubmedItem.setAttribute("pubMedId", pmid);
+                                addItem(pubmedItem);
+                            }
+                            addPublication(pubmedItem);
+                        } else if (ref.startsWith("UniProt:")) {
+                            String uniprotAcc = ref.substring(colonIndex + 1);
+
+                            Item proteinItem;
+                            if (protIdMap.containsKey(uniprotAcc)) {
+                                proteinItem = protIdMap.get(uniprotAcc);
+                            } else {
+                                proteinItem = converter.createItem("Protein");
+                                proteinItem.setAttribute("primaryAccession", uniprotAcc);
+                                proteinItem.setReference("organism", getOrganism());
+                                addItem(proteinItem);
+
+                                protIdMap.put(uniprotAcc, proteinItem);
+                            }
+                            feature.setReference("protein", proteinItem);
+                        } else {
+                            throw new RuntimeException("unknown external reference type: " + ref);
+                        }
+                    }
                 }
+            }
+        }
 
-                // Use computational description as brief description
-                if (record.getAttributes().get("computational_description") != null) {
-                    String description = record.getAttributes().get("computational_description").iterator().next();
-                    feature.setAttribute("briefDescription", description);
-                }
-                break;
-            case "transcript":
-            case "MRNA":
-            case "mRNA":
-                if ("MRNA".equalsIgnoreCase(type)) {
-                    feature.setClassName("MRNA");
-                } else {
-                    feature.setClassName("Transcript");
-                }
-
-                // Set ID
-                String dataID = record.getAttributes().get("ID").iterator().next();
-                feature.setAttribute("primaryIdentifier", dataID);
-
-                // Use parent as name
-                if (record.getAttributes().get("Name") != null) {
-                    String description = record.getAttributes().get("Name").iterator().next();
-                    feature.setAttribute("name", description);
-                }
-
-                // This is gene alias
-                if (record.getAttributes().get("symbol") != null) {
-                    String symbol = record.getAttributes().get("symbol").iterator().next();
-                    feature.setAttribute("symbol", symbol);
-                }
-                break;
-            case "exon":
-            case "CDS":
-                if ("exon".equalsIgnoreCase(type)) {
-                    feature.setClassName("Exon");
-                } else {
-                    feature.setClassName("CDS");
-                }
-
-                // Again, This might not be needed
-                String exonID = record.getAttributes().get("ID").iterator().next();
-                feature.setAttribute("primaryIdentifier", exonID);
-                break;
+        // For the following feature classes, check if the GFF3 attribute `Name` is defined.
+        // If true, assign its value to the InterMine `symbol` attribute
+        regexp = "Exon|CDS|UTR|Fragment";
+        p = Pattern.compile(regexp);
+        m = p.matcher(clsName);
+        if (m.find()) {
+            if (record.getAttributes().get("Name") != null) {
+                String name = record.getAttributes().get("Name").iterator().next();
+                feature.setAttribute("symbol", name);
+            }
         }
     }
 }
